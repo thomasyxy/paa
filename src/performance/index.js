72c802bc635c69e2ac89a55fdb825c55bbba1f95
chuwu <chuwu@myweimai.com>
2019-08-15 10:58:29 +0800

    v2

--- src/performance/index.js
+++ src/performance/index.js
@@ -1,8 +1,12 @@
 const puppeteer = require('puppeteer')
+const Analyzer = require('../analyzer')
+const analyzer = new Analyzer()
 
 module.exports = class Performance {
   constructor (opts) {
     this.opts = opts
+    this.times = 0
+    this.log = []
   }
 
   async run (opts = this.opts) {
@@ -21,8 +25,9 @@ module.exports = class Performance {
     } = opts
 
     let launchOpts = {
-      headless
+      headless,
       // args: ['--unlimited-storage', '--full-memory-crash-report']
+      args: ['--no-sandbox']
     }
 
     if (executablePath) {
@@ -49,26 +54,43 @@ module.exports = class Performance {
       settingTasks.push(tab.setUserAgent(useragent))
     }
     await Promise.all(settingTasks)
-    for (let i = 0; i < count; i++) {
-      loadTasks.push(
-        tab.goto(url, { timeout: 172800000, waitUntil: 'load' })
-      )
-      let loadHandler = () => {
-        loadEvents.push(tab.evaluate(() => {
-          let total = window.performance
-          let entries = total.getEntries()
-          return JSON.stringify({ total, entries })
-        }))
-        tab.removeListener('load', loadHandler)
+    // for (let i = 0; i < count; i++) {
+    //   loadTasks.push(
+    //     tab.goto(url, { timeout: 5000, waitUntil: 'load' })
+    //   )
+    //   let loadHandler = () => {
+    //     console.log(1);
+    //     loadEvents.push(tab.evaluate(async statisticData => {
+    //       let total = window.performance
+    //       let entries = total.getEntries()
+    //       let data = await analyzer.statistics(statisticData)
+    //       console.log('data:', JSON.stringify({ total, entries }))
+    //       outputer.output(data)
+    //     }))
+    //   }
+    // }
+    let loadHandler = async () => {
+      this.times = this.times + 1
+      const result = await tab.evaluate(() => {
+        let total = window.performance
+        let entries = total.getEntries()
+        let data = JSON.stringify({ total, entries })
+        return data
+      })
+      this.log.push(result)
+      if (this.times < count) {
+        tab.goto(url, { timeout: 5000, waitUntil: 'load' })
+        tab.once('load', loadHandler)
+      } else {
+        setTimeout(() => browser.close())
+        console.log(JSON.stringify(analyzer.statistics(this.log).total))
       }
-      tab.on('load', loadHandler)
     }
-    await Promise.all(loadTasks)
-    let performances = await Promise.all(loadEvents)
-    setTimeout(() => browser.close())
+    tab.goto(url, { timeout: 5000, waitUntil: 'load' })
+    tab.once('load', loadHandler)
     global.__hiper__.runInterval = Date.now() - startTimestamp
     // console.log(`跑完 ${global.__hiper__.url} 全部性能测试用时：${(Date.now() - startTimestamp) / 1000}s`)
     // console.log(`\n---------------------- 🚀 各项指标平均耗时（${global.__hiper__.count}次）----------------------\n`)
-    return performances
+    // return performances
   }
 }
