bbb12789d659203bf0f5a0a2ba1cdc22ad9b0661
thomasyxy <838476901@qq.com>
2020-12-08 14:16:10 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  cdnReg: /(cdn.myweimai.com|static.qstcdn.com|img.qstcdn.com|article.myweimai.com|weimai-yunyin.oss-cn-hangzhou.aliyuncs.com|dev.cdn.myweimai.com)/,
-  urlReg: /integration.m.myweimai.com/,
+  cdnReg: /(cdn.a.com|static.b.com|img.c.com)/,
+  urlReg: /baidu.com/,
   jscssReg: /(js|css)$/i,
   imgReg: /(png|jpg)$/i,
   base64Reg: /base64/i,
